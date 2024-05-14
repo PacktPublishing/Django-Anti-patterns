6ccd23f0fc5980a45d1e5ddc996f7fa7233a4f1c
jpromanonet <programming@jpromano.net>
2024-05-14 11:32:35 -0300

    ch2: class without god object

---
+++ ch2/class_without_god_object.py
@@ -0,0 +1,25 @@
+class Database:
+
+    def query(self, request):
+
+        # Database query logic
+
+        pass
+
+
+class BusinessLogic:
+
+    def process(self, data):
+
+        # Business logic processing
+
+        pass
+
+
+class PresentationLayer:
+
+    def render(self, result):
+
+        # Presentation layer rendering
+
+        pass
