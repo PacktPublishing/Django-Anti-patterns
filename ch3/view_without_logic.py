62089bb89634da0c32bb86ce83a72672b2d436a0
jpromanonet <programming@jpromano.net>
2024-05-14 14:37:14 -0300

    ch3: view without logic

---
+++ ch3/view_without_logic.py
@@ -0,0 +1,11 @@
+# views.py
+from django.shortcuts import render
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import viewsets
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+# Other views...
