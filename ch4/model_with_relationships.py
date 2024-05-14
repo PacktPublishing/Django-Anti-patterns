12a8c6a18b12cb76dcf3fa568221232b2adb148d
jpromanonet <programming@jpromano.net>
2024-05-14 16:07:47 -0300

    ch4: model with relationships

---
+++ ch4/model_with_relationships.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_date = models.DateField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
