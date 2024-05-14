3ed876dd99d440875220f841056dc85643e01fac
jpromanonet <programming@jpromano.net>
2024-05-14 10:24:29 -0300

    ch1: repetitive code example

---
+++ ch1/repetitive_code_example.py
@@ -0,0 +1,12 @@
+def format_customer_info(customer):
+
+    # Format and return customer info
+
+    return f"{customer.name}, {customer.address}"
+
+
+def format_supplier_info(supplier):
+
+    # Format and return supplier info
+
+    return f"{supplier.name}, {supplier.address}"
