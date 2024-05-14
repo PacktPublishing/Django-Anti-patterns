df00d41c3b6679867e914a0a15e2c77ffcc2b40b
jpromanonet <programming@jpromano.net>
2024-05-14 11:35:00 -0300

    ch2: fat model and fat view

--- ch2/fat_model_fat_view.py
+++ ch2/fat_model_fat_view.py
@@ -1,16 +1,9 @@
 class FatModel(models.Model):
-
     def complex_operation(self):
-
         # Complex business logic in model
-
         pass
 
-
 def thin_view(request):
-
     obj = FatModel.objects.get(id=1)
-
     result = obj.complex_operation()
-
     return HttpResponse(result)
