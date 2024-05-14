cc97a699d14856d439e156786c3e4320eca646ff
jpromanonet <programming@jpromano.net>
2024-05-14 14:36:48 -0300

    ch3: view with logic

---
+++ ch3/view_with_logic.py
@@ -0,0 +1,13 @@
+# views.py
+from django.shortcuts import render
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product_list.html', {'products': products})
+
+def product_detail(request, pk):
+    product = Product.objects.get(pk=pk)
+    return render(request, 'product_detail.html', {'product': product})
+
+# Other views...
