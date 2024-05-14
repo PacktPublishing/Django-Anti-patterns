fbd8a4e54b6552f9d4101210f6e997e4c73f3fe3
jpromanonet <programming@jpromano.net>
2024-05-14 10:15:38 -0300

    ch1: inefficient query database (refactored)

--- ch1/n1_query_problem_refactored.py
+++ ch1/n1_query_problem_refactored.py
@@ -2,6 +2,12 @@
 
 for order in Order.objects.select_related("item").all():
 
-    print(
-        order.item.name
-    )  # Item details are fetched in a single query along with orders
+    print(order.item.name)
+
+# Item details are fetched in a single query along with orders
+# Optimized version using Django's ORM
+
+
+def list_blog_posts(user):
+
+    return BlogPost.objects.filter(author=user)
